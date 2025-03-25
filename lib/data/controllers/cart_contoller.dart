c6759a17b55ee6fc91f482e70fe8fcec6339cd0d
cypher <wahbounallah00@gmail.com>
2025-03-25 15:13:13 +0100

    commaaaaaaaa

--- lib/data/controllers/cart_contoller.dart
+++ lib/data/controllers/cart_contoller.dart
@@ -208,7 +208,7 @@ class CartController extends GetxController {
           <id_carrier>$idCarrier</id_carrier>
           <id_address_delivery><![CDATA[$idAddressDelivery]]></id_address_delivery>
           <id_address_invoice><![CDATA[$idAddressDelivery]]></id_address_invoice>
-          <delivery_option>{"$idAddressDelivery":"$idCarrier"}</delivery_option> <!-- Set delivery_option -->
+          <delivery_option>{"$idAddressDelivery":"$idCarrier,"}</delivery_option> <!-- Set delivery_option -->
           <associations>
             <cart_rows>
               $cartRowsXml
